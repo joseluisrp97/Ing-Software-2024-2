ecce2fe4241d2b9711676938bd342718499ecb30
José Luis Raya Pérez <rayaperezjoseluis@gmail.com>
2024-02-08 17:10:19 -0600

    mas funciones

--- 1-tenis.py
+++ 1-tenis.py
@@ -26,4 +26,24 @@ def actualizar_puntaje(puntajes, jugador):
         puntajes[jugador] = [15, 30, 40][[0, 15, 30].index(puntajes[jugador])]  # Actualizar puntaje normalmente
     return False
 
+def solicitar_ganador_punto(mensaje, jugadores):
+    """Solicita al usuario quién gana el punto y maneja errores de entrada."""
+    while True:
+        entrada = input(mensaje)
+        try:
+            seleccion = int(entrada) - 1
+            if seleccion in [0, 1]:
+                return seleccion
+            else:
+                print(f"Por favor, ingresa un número válido. (1: {jugadores[0]}, 2: {jugadores[1]})")
+        except ValueError:
+            print("Entrada inválida. Por favor, ingresa un número.")
 
+def imprimir_marcador(sets, juegos, puntajes, jugadores):
+    """Imprime el estado actual del marcador."""
+    print(f"Sets: {jugadores[0]} {sets[0]} - {sets[1]} {jugadores[1]}")
+    print(f"Juegos (Set actual): {jugadores[0]} {juegos[0]} - {juegos[1]} {jugadores[1]}")
+    print(f"Puntos (Juego actual): {jugadores[0]} {puntajes[0]} - {puntajes[1]} {jugadores[1]}")
+def imprimir_estado_del_saque(saque, jugadores):
+    """Imprime quién tiene el saque."""
+    print(f"Saque: {jugadores[saque]}")
