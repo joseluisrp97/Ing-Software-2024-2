9ee49645591ff5672b7fa3fded9a584bb157ff63
José Luis Raya Pérez <rayaperezjoseluis@gmail.com>
2024-03-18 14:08:54 -0600

    db Fer ok

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -7,7 +7,7 @@ from controllers.ControllerRenta import rentar_blueprint
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ferfong:Develooper123!@localhost:3306/lab_ing_software'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ferfong:Develooper123!@localhost:3306/ing_soft'
 app.config.from_mapping(
     SECRET_KEY='dev'
 )
